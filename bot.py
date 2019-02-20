3c79925450528d58f147d1ebda6bd797bc280f37
Nikita Keler <47213208+RodaRolla@users.noreply.github.com>
2019-02-21 00:21:46 +0300

    Add files via upload

---
+++ bot.py
@@ -0,0 +1,45 @@
+#!python
+
+from jopajopa import superCalculator
+from telegram.ext import Updater
+from telegram.ext import CommandHandler
+from telegram.ext import MessageHandler, Filters
+import logging
+
+
+def think(ls):
+	return ''.join(ls)
+
+def start(bot, update):
+    bot.send_message(chat_id=update.message.chat_id, text="I'm a bot, please talk to me!")
+	
+def calc(bot, update, args):
+		bot.send_message(chat_id=update.message.chat_id, text="%s, it is %d " % (update.message.from_user.first_name, superCalculator(''.join(args))))
+		
+def xyecho(bot, update):
+	answer=''
+	for symb in update.message.text:
+		answer+="(%s)" % symb
+	firstN=update.message.from_user.first_name
+	lastN=update.message.from_user.last_name
+	bot.send_message(chat_id=update.message.chat_id, text="Dear %s %s. You have said fucking %s" % (firstN, lastN,answer))
+	
+if __name__ == '__main__':
+	
+	logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.DEBUG)
+	updater = Updater(token='764096237:AAFJCh0TuxxjJ47peMMjpOTCVp4S-aGJcG0',
+						request_kwargs={'proxy_url':'socks5://phobos.public.opennetwork.cc:1090',  'urllib3_proxy_kwargs': {'username': '257314152', 'password': 'cWH5NtTJ'}}) 
+	print("Create updater")
+	start_handler = CommandHandler('start', start)
+	calc_handler = CommandHandler('calc', calc, pass_args=True)
+	print("Create command handler")
+	updater.dispatcher.add_handler(start_handler)
+	updater.dispatcher.add_handler(calc_handler)
+	print("Start filters")
+	echo_handler = MessageHandler(Filters.text, xyecho)
+	updater.dispatcher.add_handler(echo_handler)
+	print("Start polling")
+	updater.start_polling()
+	print("Idle")
+	updater.idle()
+
